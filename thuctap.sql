a108e2738eeb9188b972d715b4a03fa804b476e8
ANDY <andy.nvt.vn@gmail.com>
2018-05-21 16:27:31 +0700

    update sql

---
+++ thuctap.sql
@@ -0,0 +1,375 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 21, 2018 at 11:27 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thuctap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_place` int(10) UNSIGNED NOT NULL,
+  `star` double(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2018_05_21_084732_cities', 1),
+(3, '2018_05_21_084742_place_type', 1),
+(4, '2018_05_21_084751_places', 1),
+(5, '2018_05_21_084800_place_image', 1),
+(6, '2018_05_21_084810_feedback', 1),
+(7, '2018_05_21_084819_place_location', 1),
+(8, '2018_05_21_090816_user_first_location', 1),
+(9, '2018_05_21_090822_user_last_location', 1),
+(10, '2018_05_21_090828_user_location', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `places`
+--
+
+CREATE TABLE `places` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_type` int(10) UNSIGNED NOT NULL,
+  `id_city` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `short_des` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `full_des` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `map` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `place_image`
+--
+
+CREATE TABLE `place_image` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_place` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `place_location`
+--
+
+CREATE TABLE `place_location` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_place` int(10) UNSIGNED NOT NULL,
+  `x` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `y` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `place_type`
+--
+
+CREATE TABLE `place_type` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_first_location`
+--
+
+CREATE TABLE `user_first_location` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `x` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `y` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_last_location`
+--
+
+CREATE TABLE `user_last_location` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `x` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `y` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_location`
+--
+
+CREATE TABLE `user_location` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_first` int(10) UNSIGNED NOT NULL,
+  `id_last` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `feedbacks_id_place_foreign` (`id_place`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `places`
+--
+ALTER TABLE `places`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `places_id_type_foreign` (`id_type`),
+  ADD KEY `places_id_city_foreign` (`id_city`);
+
+--
+-- Indexes for table `place_image`
+--
+ALTER TABLE `place_image`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `place_image_id_place_foreign` (`id_place`);
+
+--
+-- Indexes for table `place_location`
+--
+ALTER TABLE `place_location`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `place_location_id_place_foreign` (`id_place`);
+
+--
+-- Indexes for table `place_type`
+--
+ALTER TABLE `place_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `user_first_location`
+--
+ALTER TABLE `user_first_location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_last_location`
+--
+ALTER TABLE `user_last_location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_location`
+--
+ALTER TABLE `user_location`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_location_id_first_foreign` (`id_first`),
+  ADD KEY `user_location_id_last_foreign` (`id_last`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `places`
+--
+ALTER TABLE `places`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `place_image`
+--
+ALTER TABLE `place_image`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `place_location`
+--
+ALTER TABLE `place_location`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `place_type`
+--
+ALTER TABLE `place_type`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_first_location`
+--
+ALTER TABLE `user_first_location`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_last_location`
+--
+ALTER TABLE `user_last_location`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_location`
+--
+ALTER TABLE `user_location`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD CONSTRAINT `feedbacks_id_place_foreign` FOREIGN KEY (`id_place`) REFERENCES `places` (`id`);
+
+--
+-- Constraints for table `places`
+--
+ALTER TABLE `places`
+  ADD CONSTRAINT `places_id_city_foreign` FOREIGN KEY (`id_city`) REFERENCES `cities` (`id`),
+  ADD CONSTRAINT `places_id_type_foreign` FOREIGN KEY (`id_type`) REFERENCES `place_type` (`id`);
+
+--
+-- Constraints for table `place_image`
+--
+ALTER TABLE `place_image`
+  ADD CONSTRAINT `place_image_id_place_foreign` FOREIGN KEY (`id_place`) REFERENCES `places` (`id`);
+
+--
+-- Constraints for table `place_location`
+--
+ALTER TABLE `place_location`
+  ADD CONSTRAINT `place_location_id_place_foreign` FOREIGN KEY (`id_place`) REFERENCES `places` (`id`);
+
+--
+-- Constraints for table `user_location`
+--
+ALTER TABLE `user_location`
+  ADD CONSTRAINT `user_location_id_first_foreign` FOREIGN KEY (`id_first`) REFERENCES `user_first_location` (`id`),
+  ADD CONSTRAINT `user_location_id_last_foreign` FOREIGN KEY (`id_last`) REFERENCES `user_first_location` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
