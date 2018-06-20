ffc6dd7827d297e33bd599ca4d3ae3074a26c6e2
dobyboy <lytruonggiang2510@gmail.com>
2018-06-20 08:56:11 +0700

    k co gi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/homepage  ';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
